1896ba158fda3c3550944df9f27bddee81a27692
Theo Seidel <seidelt7@gmail.com>
2025-01-31 11:05:41 -0500

    delete

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Chat from "@/components/chat";
-
-export default function Home() {
-  return (
-    <div className="relative flex h-[calc(100vh_-_theme(spacing.16))] overflow-hidden pb-10 flex-col">
-      <Chat />
-    </div>
-  );
-}
